aac8aa45caadcadbd5bce24ea3c748b16ef2a507
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-12-22 10:35:51 -0300

    cors

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 3333;
+const cors = require('cors');
 const routes = require("./src/routes/routes");
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
