26f70782ac842b0b5facb3c4c13b5a6d2706cafc
laersonjr <58311661+laersonjr@users.noreply.github.com>
2023-04-27 14:48:41 -0300

    Update service_Perdido.js
    Acrescentado fetch e dependência paga que seja possível carregar os dados no servidor.

--- src/services/service_Perdido.js
+++ src/services/service_Perdido.js
@@ -1,3 +1,6 @@
+//Acrescentado fetch e dependência paga que seja possível carregar os dados no servidor.
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 exports.valuesPerdido = async () => {
     const phasePerdidoId = "310751593"
 
